dc2d071b927cd05981037e1f846c9e8505b5ff45
Terrill Couther <terrill.couther@gmail.com>
2023-04-12 17:33:37 -0700

    practice react assignment 2, lifting state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import ExpenseList from './Components/ExpenseList.js';
+import ExpenseListAdd from './Components/ExpenseListAdd.js';
 
 const EXPENSELIST = [
-  {"title": "Bitcoin", "amount": "204.00", "date": new Date(2022,1,20)},
-  {"title": "Ethereum", "amount": "56.50", "date": new Date(2022,5,10)},
-  {"title": "Stacks", "amount": "550.00", "date": new Date(2022,12,25)}
+  {"id":"0000001", "title": "Bitcoin", "amount": "204.00", "date": new Date(2022,1,20)},
+  {"id":"0000002", "title": "Ethereum", "amount": "56.50", "date": new Date(2022,5,10)},
+  {"id":"0000003", "title": "Stacks", "amount": "550.00", "date": new Date(2022,12,25)}
 ];
 
 function App() {
+  const addNewExpense = (enteredData)=>{
+
+    const expenseListItem = {...enteredData, id:Math.random().toString()};
+    console.log(expenseListItem);
+  };
+
   return (
     <div className="App page-container">
       <h1>My Crypto Expense List</h1>
+      <ExpenseListAdd onSubmission={addNewExpense}></ExpenseListAdd>
       <ExpenseList listdata={EXPENSELIST}></ExpenseList>
     </div>
   );
--- src/Components/ExpenseListAdd.js
+++ src/Components/ExpenseListAdd.js
@@ -1,17 +1,53 @@
-export default function ExpenseListAdd() {
+import {useState} from 'react';
+
+export default function ExpenseListAdd(props) {
+    
+    const [expenseTitle,setExpenseTitle] = useState('');
+    const [expenseAmount,setExpenseAmount] = useState('');
+    const [expenseDate,setExpenseDate] = useState('');
+
+    const submitHandler = (event)=>{
+        event.preventDefault();
+        const submittedData = {
+            expenseTitle,
+            expenseAmount,
+            expenseDate : new Date(expenseDate)
+        };
+        setExpenseTitle('');
+        setExpenseAmount('');
+        setExpenseDate('');
+
+        props.onSubmission(submittedData);
+    };
+
+    const handleChangeExpenseTitle = (event)=>{
+
+        setExpenseTitle(event.target.value);
+    };
+
+    const handleChangeExpenseAmount = (event)=>{
+
+        setExpenseAmount(event.target.value);
+    };
+
+    const handleChangeExpenseDate = (event)=>{
+
+        setExpenseDate(event.target.value);
+    };
+
     return (
-        <form className="form-container expense-list-add-form">
+        <form className="form-container expense-list-add-form" onSubmit={submitHandler}>
             <div className="form-column">
                 <label htmlFor="title">Title</label>
-                <input name="title" type="text" required />
+                <input name="title" type="text" value={expenseTitle} required onChange={handleChangeExpenseTitle} />
             </div>
             <div className="form-column">
                 <label htmlFor="amount">Amount</label>
-                <input name="amount" type="number" min="0.01" step="0.01" required />
+                <input name="amount" type="number" value={expenseAmount} min="0.01" step="0.01" required onChange={handleChangeExpenseAmount} />
             </div>
             <div className="form-column">
                 <label htmlFor="date">Date</label>
-                <input name="date" type="text" />
+                <input name="date" type="date" value={expenseDate} min="2018-12-31" max="2023-12-31" onChange={handleChangeExpenseDate} />
             </div>
             <div className="form-column"><button type="submit">Submit</button></div>
         </form>
